d3370809fe6c769f9319f78399b0ef43ab8a29eb
Andrew Manderson <21134926@student.uwa.edu.au>
2017-04-27 20:07:20 +0800

    structure in comments

--- polyFitter.R
+++ polyFitter.R
@@ -5,3 +5,19 @@
 #
 # This is intending as a starting point for, upon which we will later add
 # the ability to traverse polynomial degree via reversible jump algorithm.
+
+
+# setup will probably look something like this
+#
+# load data /librarys / other files
+#
+# setup data structures for samples / inital values
+#
+# # then within the MH loop
+# # generate a sample (need function for this), for the orthonormal parameters
+# # theta
+#
+# # then accept reject that sample with is.monotonic after converting to
+# # monomial basis
+#
+# # then calculate the MH acceptance proposal on that.
