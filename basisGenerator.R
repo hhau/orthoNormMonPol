90a7b106140e682f5f23811ae045655c1df651a8
hhau <a.manderson@live.co.uk>
2017-04-27 19:17:14 +0800

    inital commit, vague attempt a script setup, likely to change

---
+++ basisGenerator.R
@@ -0,0 +1,4 @@
+# Supporting functions for OrthoNormalMonPol
+# AAM / hhau 27/04/17
+
+library(MonoPoly)
---
+++ polyFitter.R
@@ -0,0 +1,7 @@
+# AAM / hhau 27/04/17
+#
+# Orhtonormal Bayesian Monotonic polynomial using metropolis hastings
+# and a renormalised QR decomposition
+#
+# This is intending as a starting point for, upon which we will later add
+# the ability to traverse polynomial degree via reversible jump algorithm.
